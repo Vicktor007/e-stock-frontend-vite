2b9dddb93d53486f0f5ab6835f7152d249a42de6
Vicktor007 <vicktord007@gmail.com>
2024-01-12 17:49:26 +0100

    save

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,7 +11,7 @@ const Sidebar = ({ children }) => {
 
   
 
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   const navigate = useNavigate();
 
