f6c37e8a8b84856ffaf0d1dd06021c1e3af77ce9
Vicktor007 <vicktord007@gmail.com>
2024-01-26 09:21:53 +0100

    save

--- src/components/product/productDetail/LimitedProductDetails.jsx
+++ src/components/product/productDetail/LimitedProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { getProductDetails } from "../../../redux/features/product/productSlice";
